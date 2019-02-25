1c11bef1989959caafb464a059738a24c45123bf
Connor Isogai <MXCapturing@gmail.com>
2019-02-25 14:17:39 +1300

    Unity Files

---
+++ Friend Ship/Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour {
+
+    public List<GameObject> invItem = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
